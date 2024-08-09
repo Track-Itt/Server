ad35395e34dd59102248ef71e64f0804b9f76b2e
kota-karthik <kotakarthik2307@gmail.com>
2024-08-10 02:36:58 +0530

    middlewares: added errorMiddlewares

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const colors = require('colors');
 const cors = require('cors');
-// const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 
 const app = express();
 dotenv.config();
@@ -18,8 +18,8 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 
-// app.use(notFound);
-// app.use(errorHandler);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running at http://localhost:${PORT}`.yellow.bold);
