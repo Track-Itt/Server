f1b9aad1412af3cd55cd54cad2f09ebfa2eae18a
kota-karthik <kotakarthik2307@gmail.com>
2024-08-14 10:49:15 +0530

    productController: added updateProductCount controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -44,5 +44,39 @@ const addProduct=asyncHandler(async(req,res)=>{
         res.status(400).json(error.message);
     }
 })
-
-module.exports={addProduct};
+const updateProductCount=asyncHandler(async(req,res)=>{
+    const {productId,operation,count}=req.body;
+    if(!productId || !operation || !count){
+        return res.status(400).json("Please Fill all the fields!");
+    }
+    try{
+        var product=await Product.findById(productId);
+        if(!product){
+            return res.status(400).json("Product doesn't exist!");
+        }
+        if(operation=='+'){
+            product=await Product.findByIdAndUpdate(
+                productId,
+                { $inc: { count: count } },
+                { new: true }
+            );
+        }else if(operation=='-'){
+            if((product.count-count)<0){
+                return res.status(400).json("Insufficient product count! Cannot decrease below 0."); 
+            }else{
+                product=await Product.findByIdAndUpdate(
+                    productId,
+                    { $inc: { count: -count } },
+                    { new: true }
+                );
+            }
+         
+        }else{
+            return res.status(400).json("Invalid operation! Use '+' to increase or '-' to decrease the count."); 
+        }
+        res.status(200).json(product);
+    }catch(error){
+        res.status(400).json(error.message);
+    }
+})
+module.exports={addProduct,updateProductCount};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {addProduct} = require("../controllers/productController");
+const {addProduct,updateProductCount} = require("../controllers/productController");
 const { protect } = require("../middlewares/authMiddleware");
 
 router.post('/addProduct',protect,addProduct);
+router.patch('/updateProductCount',protect,updateProductCount);
 
 module.exports=router;
